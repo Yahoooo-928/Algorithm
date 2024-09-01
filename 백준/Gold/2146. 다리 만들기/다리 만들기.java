352b6f7f44807817721616c532aae210940522c0
kodongyeon <90126516+DDDongyeon@users.noreply.github.com>
2024-09-01 15:08:15 +0900

    [Gold III] Title: 다리 만들기, Time: 204 ms, Memory: 135732 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,105 +1,108 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-	
-	static int n, ans;
+	static int n, answer;
 	static int[][] map;
-	static boolean[][] v;
-	static int[] di = {-1,1,0,0};
-	static int[] dj = {0,0,-1,1};
-	
+	static boolean[][] visited;
+	static int[] di = {0,0,1,-1};
+	static int[] dj = {1,-1,0,0};
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		n = Integer.parseInt(br.readLine());
-		
 		map = new int[n][n];
 		
-		for(int i=0; i<n; i++){
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=0; j<n; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j]=Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		// 섬이 모두 1로 표시되어 구분이 어려우므로 
-		// 각각 다른 숫자로 표시해준다. 2부터 시작
-		int idx = 2;
-		v = new boolean[n][n];
-		for(int i=0; i<n; i++){
+		
+		answer = Integer.MAX_VALUE;
+		// map에서 섬을 나눠서 표시해주자
+		visited = new boolean[n][n];
+		int num = 1;
+		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
-				if(map[i][j]==1 && !v[i][j]) {
-					bfs(i,j,idx);
-					idx++;
-				}
-			}
-		}
-		// 다리를 놓을 수 있는 경우를 구한다.
-		ans = Integer.MAX_VALUE;
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++) {
-				if(map[i][j]>1) {
-					v = new boolean[n][n];
-					search(i,j,0);
+				if(!visited[i][j] && map[i][j]==1) {
+					bfs(i,j,num);
+					num++;
 				}
 			}
 		}
 		
-		System.out.println(ans);
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(map[i][j]!=0) {
+					visited = new boolean[n][n];
+					start(i,j,map[i][j]);
+				}
+			}
+		}
+		
+		System.out.println(answer);
 		
 		br.close();
 	}
-
-	static void search(int i, int j, int cnt) {
+	
+	private static void start(int i, int j, int num) {
 		ArrayDeque<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {i,j,cnt});
-		v[i][j] = true;
+		visited[i][j] = true;
+		q.offer(new int[] {i,j,0});
 		
 		while(!q.isEmpty()) {
-			int[] ij = q.poll();
+			int[] current = q.poll();
+			int x = current[0];
+			int y = current[1];
+			int c = current[2];
+			
+//			System.out.println(x+" " +y+" "+c);
+			if(map[x][y]!=0 && map[x][y]!=num) {
+				answer = Math.min(answer, c-1);
+//				System.out.println(answer+"!");
+				break;
+			}
+			
 			
 			for(int d=0; d<4; d++) {
-				int nx = ij[0]+di[d];
-				int ny = ij[1]+dj[d];
-				
-				if(nx>=0 && nx<n && ny>=0 && ny<n && !v[nx][ny]) {
-					if(map[nx][ny]>1 && map[nx][ny]!=map[i][j]) {
-						ans = Math.min(ans, ij[2]);
-					}else if(map[nx][ny]==0) {
-						v[nx][ny]=true;
-						if(ij[2]+1<=ans) {
-							q.offer(new int[] {nx,ny,ij[2]+1});
+				int nx = x+di[d];
+				int ny = y+dj[d];
+				if(nx>=0 && nx<n && ny>=0 && ny<n && !visited[nx][ny]) {
+						if(map[nx][ny]!=num) {
+							visited[nx][ny]=true;
+							q.offer(new int[] {nx,ny,c+1});
 						}
 					}
 				}
-			}
-			
-		}
-		
-		
+			}	
 	}
+	
 
-	static void bfs(int i, int j, int idx) {
+	private static void bfs(int i, int j, int num) {
 		ArrayDeque<int[]> q = new ArrayDeque<>();
+		visited[i][j] = true;
 		q.offer(new int[] {i,j});
-		v[i][j] = true;
-		map[i][j]= idx;
+		map[i][j]=num;
 		
 		while(!q.isEmpty()) {
-			int[] ij = q.poll();
+			int[] current = q.poll();
+			int x = current[0];
+			int y = current[1];
 			
 			for(int d=0; d<4; d++) {
-				int nx = ij[0]+di[d];
-				int ny = ij[1]+dj[d];
-				if(nx>=0 && nx<n && ny>=0 && ny<n && !v[nx][ny] && map[nx][ny]==1) {
-					map[nx][ny]=idx;
-					v[nx][ny]=true;
-					q.offer(new int[] {nx,ny});
+				int nx = x+di[d];
+				int ny = y+dj[d];
+				if(nx>=0 && nx<n && ny>=0 && ny<n && !visited[nx][ny]) {
+					if(map[nx][ny]==1) {
+						visited[nx][ny]=true;
+						q.offer(new int[] {nx,ny});
+						map[nx][ny]=num;
+					}
 				}
 			}
-			
 		}
+		
 	}
 
 }
